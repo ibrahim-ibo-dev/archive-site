19e5d2518a87feb9ac64ffe1e9bd4188e710d46c
imxoshnawm <imxoshnawm@users.noreply.github.com>
2026-05-18 00:38:05 +0300

    Add Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,6 +70,7 @@ export const metadata: Metadata = {
 import SmoothScroll from "@/components/SmoothScroll";
 import CustomCursor from "@/components/CustomCursor";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({
   children,
@@ -82,6 +83,7 @@ export default function RootLayout({
         <CustomCursor />
         <SmoothScroll>{children}</SmoothScroll>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
